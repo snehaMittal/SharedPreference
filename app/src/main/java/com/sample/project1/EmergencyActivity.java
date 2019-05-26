80918101ef14ddbaec397f096ebb1abeef2f25c6
Sneha Mittal <mittalsneha.1403@gmail.com>
2019-05-26 15:54:05 +0530

    message

---
+++ app/src/main/java/com/sample/project1/EmergencyActivity.java
@@ -0,0 +1,90 @@
+package com.sample.project1;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class EmergencyActivity extends AppCompatActivity {
+
+    Set<String> set , set2;
+    String array[] , phone[];
+    SharedPreferences sharedPreferences ;
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Phone = "phoneKey";
+
+    ListView listView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency);
+
+        sharedPreferences = getSharedPreferences(mypreference , Context.MODE_PRIVATE);
+        set = new HashSet<>();
+        set = sharedPreferences.getStringSet(Name , null);
+        array = new String[set.size()];
+        set2 = new HashSet<>();
+        set2 = sharedPreferences.getStringSet(Phone , null);
+        phone = new String[set2.size()];
+        int k=0;
+        for (String i : set){
+            array[k] = i;
+            k++;
+        }
+        int l=0 ;
+        for (String i : set2){
+            phone[l] = i;
+            l++;
+        }
+        listView = findViewById(R.id.listView);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return array.length ;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.custom_layout,null);
+            TextView name = convertView.findViewById(R.id.textView3);
+            TextView phoneT = convertView.findViewById(R.id.textView4);
+            name.setText(array[position]);
+            phoneT.setText(phone[position]);
+            return convertView;
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/sample/project1/MainActivity.java
@@ -0,0 +1,108 @@
+package com.sample.project1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button emergency , add ;
+    SharedPreferences sharedPreferences ;
+    private TextView resultText;
+
+    Set<String> nameSet, phoneSet ;
+
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Phone = "phoneKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        emergency = findViewById(R.id.emergency);
+        add = findViewById(R.id.add);
+        sharedPreferences = getSharedPreferences(mypreference , Context.MODE_PRIVATE);
+
+        nameSet = new HashSet<>();
+        phoneSet = new HashSet<>();
+
+        emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent( MainActivity.this , EmergencyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInputDialog();
+            }
+        });
+    }
+
+    protected void showInputDialog() {
+
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.input_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
+        final EditText editText2 = (EditText) promptView.findViewById(R.id.edittext2);
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        String str1 = editText.getText().toString() ;
+                        String str2 = editText2.getText().toString() ;
+                        if (str1.trim().equals("")){
+                            Toast.makeText(MainActivity.this, "Please enter the name ", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (str2.trim().equals("")){
+                            Toast.makeText(MainActivity.this, "Please enter phone number ", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (str2.length() != 10){
+                            Toast.makeText(MainActivity.this , "Enter Correct Phone Number", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            nameSet.add(str1 );
+                            editor.putStringSet(Name , nameSet);
+                            phoneSet.add(str2);
+                            editor.putStringSet(Phone , phoneSet);
+                            editor.apply();
+                            Toast.makeText(MainActivity.this, " Contact Saved", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+}
